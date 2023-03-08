36b3a0f2c7ce996a1abba44a867938d332127055
Kyle McCall <kylem147@gmail.com>
2023-03-07 21:00:50 -0800

    start layout

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SynthLayout from "./components/SynthLayout/SynthLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <SynthLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/SynthLayout/SynthLayout.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SynthLayout() {
+  return <div className="synth-container">synth</div>;
+}

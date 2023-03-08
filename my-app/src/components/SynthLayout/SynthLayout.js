9cd265c79961f41a2cd189912be721355470ae0a
Kyle McCall <kylem147@gmail.com>
2023-03-07 21:38:56 -0800

    play button generates sounds

--- my-app/src/components/SynthLayout/SynthLayout.js
+++ my-app/src/components/SynthLayout/SynthLayout.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SynthLayout.css";
 
 export default function SynthLayout() {
-  return <div className="synth-container">synth</div>;
+  const [waveform, setWaveform] = useState("");
+  const ctx = new AudioContext();
+
+  const osc = ctx.createOscillator();
+
+  osc.connect(ctx.destination);
+
+  const startOsc = () => {
+    osc.type = waveform;
+    osc.start();
+
+    setTimeout(() => {
+      osc.stop();
+    }, 2000);
+  };
+
+  // const buffer = ctx.createBuffer(1, ctx.sampleRate * 1, ctx.sampleRate);
+
+  // const channelData = buffer.getChannelData(0);
+
+  return (
+    <div className="synth-container">
+      <select
+        onChange={(e) => {
+          setWaveform(e.target.value);
+          console.log(waveform);
+        }}
+      >
+        <option value="square">Square</option>
+        <option value="triangle">Triangle</option>
+        <option value="sawtooth">Saw</option>
+        <option value="sine">Sine</option>
+      </select>
+      <button onClick={startOsc}>Play</button>
+    </div>
+  );
 }
